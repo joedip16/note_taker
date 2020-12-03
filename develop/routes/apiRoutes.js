1579ffa69c5798484ba319e8adc7ebf1b11df36b
joedip16 <joedippolito16@gmail.com>
2020-12-03 16:22:54 -0500

    set up apiRoutes file

---
+++ develop/routes/apiRoutes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const store = require("../db/store");
+
+router.get("/notes", (req, res) => {
+    store
+    .getNotes()
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
