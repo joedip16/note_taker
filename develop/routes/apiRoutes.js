c4a4a20d75eabb195efa482cb44020403533c7b6
joedip16 <joedippolito16@gmail.com>
2020-12-04 17:36:03 -0500

    working on POST

--- develop/routes/apiRoutes.js
+++ develop/routes/apiRoutes.js
@@ -8,6 +8,23 @@ router.get("/notes", (req, res) => {
     .catch((err) => res.status(500).json(err));
 });
 
+//post "/api/notes"
+//use addNote in the store object
+
+app.post("/api/notes", function(req, res) {
+    var newPost = req.body;
+  
+    console.log(newPost);
+  
+    notes.push(newNote);
+  
+    res.json(newNote);
+}),
+
+//delete "/api/notes" deletes the note with an id equal to req.params.id
+//use removeNote method in the store object
+//your code here
+
 
 
 
