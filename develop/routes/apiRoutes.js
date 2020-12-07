587098aa1cbc0cf4bf64eb7ebe384bef58d1298b
joedip16 <joedippolito16@gmail.com>
2020-12-07 16:35:07 -0500

    working on post

--- develop/routes/apiRoutes.js
+++ develop/routes/apiRoutes.js
@@ -11,30 +11,24 @@ router.get("/notes", (req, res) => {
 //post "/api/notes"
 //use addNote in the store object
 
-app.post("/api/notes", function(req, res) {
-    var newPost = req.body;
-  
-    console.log(newPost);
-  
-    notes.push(newNote);
-  
-    res.json(newNote);
-}),
+router.post("/notes", function(req, res) {
+    store
+    .addNote(req.body)
+    console.log(req.body)
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err))
+});
 
 //delete "/api/notes" deletes the note with an id equal to req.params.id
 //use removeNote method in the store object
 //your code here
 
-
-
-
-
-
-
-
-
-
-
-
+router.delete("/notes", function(req, res) {
+    store
+    .removeNote(req.body)
+    console.log(req.body)
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err));
+});
 
 module.exports = router;
